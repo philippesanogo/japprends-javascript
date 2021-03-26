13e8c6e00f51715209a368af23879c58eb737080
philippe sanogo <philippe.sanogo@gmail.com>
2021-03-26 12:27:23 +0100

    Create an object stylo

---
+++ object.js
@@ -0,0 +1,17 @@
+// Creation de object.stylo
+
+const stylo = {
+	taille: "petit",
+	couleur: "rose",
+	style: "encre"
+}
+
+// Affiche object.stylo dans la console
+console.log("Affiche object.stylo dans la console:")
+console.log(stylo);
+
+
+// separation
+console.log('');
+
+
