d48e17da1eca93bafe4bc609ceefc2e77a841aaa
philippe sanogo <philippe.sanogo@gmail.com>
2021-03-26 15:28:14 +0100

    Add menus:array in exercize file

--- object-exercize.js
+++ object-exercize.js
@@ -14,14 +14,24 @@ const cafe = {
 	businessHours: {
 		opening: "10H00",
 		closing: "20H00"
-	}	
-}
+	},
+	menus: ["Chocolat viennois", "Caffè latte", "Vanhoutten", "Expresso"]	
+};
 
 
 // Affiche les informations à propos de la booutique
 
 console.log("Affiche les informations à propos de la booutique:");
 
+console.log('');
+
 console.log("NOM: " + cafe.name);
 console.log("Ouverture: " + cafe.businessHours.opening);
 console.log("Fermeture: " + cafe.businessHours.closing);
+
+console.log(`----------------------------`);
+console.log("Au menu:");
+for (let i = 0; i < cafe.menus.length; i++) {
+	console.log(cafe.menus[i]);
+}
+
