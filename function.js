de92f62328edd17ceb2e51e657f583ff722e228c
philippe sanogo <philippe.sanogo@gmail.com>
2021-03-28 18:52:36 +0200

    Add some code in function.js

---
+++ function.js
@@ -0,0 +1,34 @@
+// Definir une la fonction: dire bonjour
+console.log(`Definir la fonction: dire bonjour
+Appel de la fonction dire bonjour:`);
+console.log("");
+
+const greeting = function () {
+	console.log("Hello World!");
+};
+
+// Appel de la fonction dire bonjour
+greeting();
+
+
+console.log('');
+
+
+// Definir une fonction fléchée et l'afficher dans la console 
+
+console.log("Definir une fonction fléchée et l'afficher dans la console:");
+
+const introduce = () => {
+	console.log("Hi, my name is Hello");
+};
+
+introduce();
+
+
+// Definir une fonction fléchée intitulée: sayHello
+
+const sayHello = () => {
+	console.log("Hello World!");
+};
+
+sayHello();
