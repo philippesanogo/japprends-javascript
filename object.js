8b03354a78e8b9486a19e5eaccc32034972d06c9
philippe sanogo <philippe.sanogo@gmail.com>
2021-03-29 02:00:46 +0200

    Update object.js and index.html

--- object.js
+++ object.js
@@ -26,4 +26,14 @@ console.log('');
 // Remplacer la valeur de la propriété style actuelle par "bille" et affiche dans la console l'objet stylo
 stylo.style = "bille";
 console.log(stylo);
+console.log("");
+// Creer un objet user et l'afficher dans la console
+console.log("Creer un objet user et l'afficher dans la console:");
+const user = {
+	name: "Ken the ninja",
+	age: 35
+};
+console.log("objet user:");
+console.log(user.name);
+console.log(user.age);
 
