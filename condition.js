1449fb73b4db11bcea927e8b22115363c42a3868
philippe sanogo <philippe.sanogo@gmail.com>
2021-03-31 23:43:01 +0200

    Add condition.js and update object-exercize.js

---
+++ condition.js
@@ -0,0 +1,14 @@
+/**
+ * Introduction au condition
+ */
+
+const light = true;
+const isOn =! light;
+
+if (light === isOn) {
+    console.log("Lumière allumée");
+} else {
+    console.log("Lumière éteinte");
+}
+
+// voir résultat dans la console
