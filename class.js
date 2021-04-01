9a856019121d5454c6787f0c68503f03ec989d17
philippe sanogo <philippe.sanogo@gmail.com>
2021-04-01 02:09:09 +0200

    update class.js

--- class.js
+++ class.js
@@ -1,75 +1,77 @@
-// Definir la classe User et Affiche une instance vide de la classe dans la console
-console.log("Definir la classe User et Affiche une instance vide de la classe dans la console:");
+/**
+ * EXO 1: créer la class USER et affiche dans la console
+ */
+console.log("EXO 1:");
 
-class User {}   
+class User {
+
+}
+
 const user = new User();
 console.log(user);
-// Fin
 
-// separation
-console.log("");
+console.log("----------------------------------");
 
-// Definir la classe Animal
-class Animal {}
-// Definir une instance de Animal en l'assignant dans un const
-console.log("Definir une instance de Animal en l'assignant dans un const:");
+/**
+ * EXO 2: ajoute un CONSTRUCTOR dans la CLASS Animal
+ * - ajoute du code dans CONSTRUCTOR
+ * - affiche une instance de Animal dans la console
+ */
+console.log("EXO 2:");
+
+class Animal {
+    constructor() {
+        console.log("Hello");
+    }
+}
+
 const animal = new Animal();
-// Affiche dans la console la constante
-console.log(animal);
-// Fin
 
-// separation
+console.log("----------------------------------");
+console.log("EXO 3:");
+
+/**
+ * EXO 3: créer la class Car
+ * - ajoute un CONSTRUCTOR
+ * - dans CONSTRUCTOR ajoute une PROPERTY name dont la VALUE est Volvo
+ * - affiche le nom de la marque et le type de moteur dans la console apres avoir creer une instance de Car
+ */
+
+/**
+ * EXO 4: ajoute 2 arguments à CONSTRUCTOR
+ * - change la marque et le type de moteur de l'objet car (Mazda et essence)
+ * - creer une autre instance de Car et affiche dans la console mercedes et diesel
+ */
+
+/**
+ * EXO 5: ajoute la METHOD rouler dans le CONSTRUCTOR
+ */
+
+class Car {
+    constructor (name, typeOfEngine) {
+        this.name = name;
+        this.typeOfEngine = typeOfEngine;
+    }
+
+    drive () {
+        console.log("roule vite");
+    }
+}
+
+const car = new Car("Mazda", "essence");
+console.log(car.name);
+console.log(car.typeOfEngine);
+
 console.log("");
 
+console.log("EXO 4:");
+const merco = new Car("Mercedes", "Diesel");
+console.log(`Marque: ${merco.name}`);
+console.log(`Moteur: ${merco.typeOfEngine}`);
 
-// constructor
-console.log("constructor:");
-class Voiture {
-    constructor () {
-        console.log(`Voiture`);
-    }
-}
-
-const voiture1 = new Voiture();
-const voiture2 = new Voiture();
-// Fin
-
-
-// separation
 console.log("");
-
-
-// Utilisation de this dans constructor
-const phrase = "Ma future maison aura";
-class MaisonType {
-    constructor (pieces, fenetres, portes) {
-        this.pieces = pieces;
-        this.fenetres = fenetres;
-        this.portes = portes;
-    }
-}
-
-const maison = new MaisonType(7, 18, 4);
-console.log(`${phrase} ${maison.pieces} pièces`);
-console.log(`${phrase} ${maison.fenetres} fenêtres`);
-console.log(`${phrase} ${maison.portes} portes`);
-// Fin
-
-
-console.log('');
-
-// un autre exemple
-class Velo {
-    constructor (marque, type, prix) {
-        this.marque = marque;
-        this.type = type;
-        this.prix = prix;
-    }
-}
-
-const velo1 = new Velo("Mitsubishi", "MountainBike", 500);
-
-console.log("Marque: " + velo1.marque);
-console.log("Type: " + velo1.type);
-console.log("Prix: " + velo1.prix);
-
+console.log("EXO 5:");
+const renault = new Car("espace", "Diesel");
+console.log("Marque: " + renault.name);
+console.log("Moteur: " + renault.typeOfEngine);
+renault.drive();
